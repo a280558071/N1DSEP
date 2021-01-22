25bedd3fcbaa93cb243791ac2d8dce7a35753b2f
Shen Xinwei <shenxwtsinghua@126.com>
2021-01-22 21:19:31 +0800

    Update Case16_noV.m

--- Case16_noV.m
+++ Case16_noV.m
@@ -265,37 +265,37 @@ for i=15:20 % Contigency i happens
 end
 
 %% find where rt!=0 and plot the Contigency senario
-[xr,yr]=find(s_rt1~=0);
-j=0;
-for i=yr'
-    j=j+1;
-    figure;
-    s_temp=s;
-    t_temp=t;
-    N_RLoads=find(N_Loads~=xr(j));
-    for l=1:L
-        if s_be1(l,i,2)==1
-            s1=s_temp(l);
-            s_temp(l)=t(l);
-            t_temp(l)=s1;
-        end
-    end
-    Gi=graph(s_temp,t_temp);
-    pir(j)=plot(Gi,'Layout','force');
-    pir(j).XData=CXY(1,:);
-    pir(j).YData=CXY(2,:);
-    labelnode(pir,N_Subs,{'13','14','15','16'});
-    highlight(pir,N_Loads,'NodeColor','y','Markersize',20,'NodeFontSize',20);
-    highlight(pir,N_Subs,'Marker','s','NodeColor','c','Markersize',30,'NodeFontSize',40);
-    highlight(pir(j),s_temp(find(s_x1==0)),t_temp(find(s_x1==0)),'LineStyle','--','LineWidth',1);
-    highlight(pir(j),s_temp(find(s_x1==1)),t_temp(find(s_x1==1)),'LineWidth',4); % bold line denotes the newly-built line
-    highlight(pir(j),s_temp(i),t_temp(i),'EdgeColor','k','LineStyle','-','LineWidth',4);  % black line denotes the outage line
-    highlight(pir(j),s_temp(find(s_y1(:,i)==1)),t_temp(find(s_y1(:,i)==1)),'EdgeColor','r','LineWidth',6,'LineStyle','-');
-    labelnode(pir(j),xr(j),{[num2str(Load(xr(j))) ' Lost ' num2str(s_rt1(xr(j),yr(j)))]});
-    for k=1:length(N_RLoads)
-        labelnode(pir(j),N_RLoads(k),num2str(Load(N_RLoads(k))));
-    end
-    title(['Contigency ' num2str(i) ' happens, shedding load in node ', num2str(xr(j))]);
-    text(pir(i).XData, pir(i).YData, pir(i).NodeLabel,'HorizontalAlignment', 'center','FontSize', 15); % put nodes' label in right position.
-    pir(i).NodeLabel={};
-end
+% [xr,yr]=find(s_rt1~=0);
+% j=0;
+% for i=yr'
+%     j=j+1;
+%     figure;
+%     s_temp=s;
+%     t_temp=t;
+%     N_RLoads=find(N_Loads~=xr(j));
+%     for l=1:L
+%         if s_be1(l,i,2)==1
+%             s1=s_temp(l);
+%             s_temp(l)=t(l);
+%             t_temp(l)=s1;
+%         end
+%     end
+%     Gi=graph(s_temp,t_temp);
+%     pir(j)=plot(Gi,'Layout','force');
+%     pir(j).XData=CXY(1,:);
+%     pir(j).YData=CXY(2,:);
+%     labelnode(pir,N_Subs,{'13','14','15','16'});
+%     highlight(pir,N_Loads,'NodeColor','y','Markersize',20,'NodeFontSize',20);
+%     highlight(pir,N_Subs,'Marker','s','NodeColor','c','Markersize',30,'NodeFontSize',40);
+%     highlight(pir(j),s_temp(find(s_x1==0)),t_temp(find(s_x1==0)),'LineStyle','--','LineWidth',1);
+%     highlight(pir(j),s_temp(find(s_x1==1)),t_temp(find(s_x1==1)),'LineWidth',4); % bold line denotes the newly-built line
+%     highlight(pir(j),s_temp(i),t_temp(i),'EdgeColor','k','LineStyle','-','LineWidth',4);  % black line denotes the outage line
+%     highlight(pir(j),s_temp(find(s_y1(:,i)==1)),t_temp(find(s_y1(:,i)==1)),'EdgeColor','r','LineWidth',6,'LineStyle','-');
+%     labelnode(pir(j),xr(j),{[num2str(Load(xr(j))) ' Lost ' num2str(s_rt1(xr(j),yr(j)))]});
+%     for k=1:length(N_RLoads)
+%         labelnode(pir(j),N_RLoads(k),num2str(Load(N_RLoads(k))));
+%     end
+%     title(['Contigency ' num2str(i) ' happens, shedding load in node ', num2str(xr(j))]);
+%     text(pir(i).XData, pir(i).YData, pir(i).NodeLabel,'HorizontalAlignment', 'center','FontSize', 15); % put nodes' label in right position.
+%     pir(i).NodeLabel={};
+% end
