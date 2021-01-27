fccd7c4fd1f8f53d6c3de5971da5e5cf2b1d073d
Shen Xinwei <shenxwtsinghua@126.com>
2021-01-27 17:35:37 +0800

    Update Case25_withV.m

--- Case25_withV.m
+++ Case25_withV.m
@@ -123,6 +123,7 @@ v_max=1.05;
 % f_Max=ones(L,1)*50;
 g_Sub_Max=50;
 M=1e7;
+M2=1e3;
 Cost=ConsInf(:,6).*ConsInf(:,3);
 x=binvar(L,1,'full');     %Vars for line construction, x(i,1)==1 dentoes that line i is constructed.
 y=binvar(L,L,'full');   %Vars for line operation flag in different contigencies, y(line operation,Cont_l)==0
@@ -204,7 +205,7 @@ Cons=[Cons,Cons_ST];
 %% Cons6: Degree of Each Node Constr.
 Cons_De=[];
 for i=N_Loads
-    Cons_De=[Cons_De,sum(x([find(s==i);find(t==i)]))>=3];
+    Cons_De=[Cons_De,sum(x([find(s==i);find(t==i)]))>=2];
 end
 Cons=[Cons,Cons_De];
 % size(Cons_De)
@@ -237,22 +238,22 @@ Cons=[Cons,Cons_Sub];
 % Cons_Links=[sum(x)>=a]; % a is a magical number that limits the reliability of distribution network.
 % Cons=[Cons,Cons_Links];
 %% Cons11: Voltage limits
-% Cons_Vol=[];
-% for C_l=1:L
-%     Cons_Vol=[Cons_Vol,v_min<=v(:,C_l)<=v_max];
-%     Cons_Vol=[Cons_Vol,v(N_Subs,C_l)==v_max];
-%     Cons_Vol=[Cons_Vol,-(1-y(:,C_l))*M<=f(:,C_l).*z-Full_I'*v(:,C_l)<=(1-y(:,C_l))*M];
-% end
-% Cons=[Cons,Cons_Vol];
+Cons_Vol=[];
+for C_l=1:L
+    Cons_Vol=[Cons_Vol,v_min<=v(:,C_l)<=v_max];
+    Cons_Vol=[Cons_Vol,v(N_Subs,C_l)==v_max];
+    Cons_Vol=[Cons_Vol,-(1-y(:,C_l))*M2<=f(:,C_l).*z-Full_I'*v(:,C_l)<=(1-y(:,C_l))*M2];
+end
+Cons=[Cons,Cons_Vol];
 %% Cons12: Best Known Obj
 % Cons=[Cons, Obj>=94858];
 %% Set initial guess of x,y and be_Nodes to values in "Case25_noV_nox0_withDE3_realf12.mat"
-ops=sdpsettings('solver','cplex','verbose',2,'cplex.mip.display',3,'usex0',0,'cplex.mip.tolerances.mipgap',5e-2);%,'cplex.mip.limits.cutpasses',-1,'cplex.mip.tolerances.integrality',1e-8);
-% ops=sdpsettings('solver','gurobi','gurobi.MIPGap',5e-2,'gurobi.Heuristics',0,'gurobi.Cuts',0,'usex0',0);% 
-% load('Case25_noV_nox0_withDE3_Gap5_Gurobi.mat','s_x1','s_y1','s_be1');
-% assign(x,s_x1);
-% assign(y,s_y1);
-% assign(be,s_be1);
+ops=sdpsettings('solver','cplex','verbose',2,'cplex.mip.display',3,'usex0',1,'cplex.mip.tolerances.mipgap',5e-2);%,'cplex.mip.limits.cutpasses',-1,'cplex.mip.tolerances.integrality',1e-8);
+% ops=sdpsettings('solver','gurobi','gurobi.MIPGap',5e-2,'usex0',0);% 'gurobi.Heuristics',0,'gurobi.Cuts',0,
+load('Case25_Cplex_newM_V_nox0_withDE3_Gap5.mat','s_x1','s_y1','s_be1');
+assign(x,s_x1);
+assign(y,s_y1);
+assign(be,s_be1);
 
 %% solve the problem
 sol1=optimize(Cons,Obj,ops);
@@ -262,9 +263,10 @@ s_y1=value(y);
 s_be1=value(be);
 s_f1=value(f);
 s_rt1=value(rt);
+s_v1=value(v);
 s_g_Sub1=value(g_Sub);
 s_Obj1=value(Obj);
-save('Case25_noV_nox0_withDE3_Gap5_Cplex');
+save('Case25_Cplex_newM_V_nox0_withDE3_Gap5');
 %% Highlight the lines to be bulit and plot all the operation conditions
 for i=1:5 % Contigency i happens
     figure;
@@ -298,7 +300,7 @@ for i=1:5 % Contigency i happens
 end
 
 %% find where rt!=0 and plot the Contigency senario
-[xr,yr]=find(s_rt1~=0);
+[xr,yr]=find(s_rt1>=1e-6);
 j=0;
 for i=yr'
     j=j+1;
@@ -317,9 +319,9 @@ for i=yr'
     pir(j)=plot(Gi,'Layout','force');
     pir(j).XData=CXY(1,:);
     pir(j).YData=CXY(2,:);
-    labelnode(pir,N_Subs,{'22','23','24','25'});
-    highlight(pir,N_Loads,'NodeColor','y','Markersize',20,'NodeFontSize',20);
-    highlight(pir,N_Subs,'Marker','s','NodeColor','c','Markersize',30,'NodeFontSize',40);
+    labelnode(pir(j),N_Subs,{'22','23','24','25'});
+    highlight(pir(j),N_Loads,'NodeColor','y','Markersize',20,'NodeFontSize',20);
+    highlight(pir(j),N_Subs,'Marker','s','NodeColor','c','Markersize',30,'NodeFontSize',40);
     highlight(pir(j),s_temp(find(s_x1==0)),t_temp(find(s_x1==0)),'LineStyle','--','LineWidth',1);
     highlight(pir(j),s_temp(find(s_x1==1)),t_temp(find(s_x1==1)),'LineWidth',4); % bold line denotes the newly-built line
     highlight(pir(j),s_temp(i),t_temp(i),'EdgeColor','k','LineStyle','-','LineWidth',4);  % black line denotes the outage line
@@ -328,7 +330,7 @@ for i=yr'
     for k=1:length(N_RLoads)
         labelnode(pir(j),N_RLoads(k),num2str(Load(N_RLoads(k))));
     end
-    title(['Contigency ' num2str(i) ' happens, shedding load in node ', num2str(xr(j))]);
-    text(pir(i).XData, pir(i).YData, pir(i).NodeLabel,'HorizontalAlignment', 'center','FontSize', 15); % put nodes' label in right position.
-    pir(i).NodeLabel={};
+    title(['Contigency ' num2str(j) ' happens, shedding load in node ', num2str(xr(j))]);
+    text(pir(j).XData, pir(j).YData, pir(j).NodeLabel,'HorizontalAlignment', 'center','FontSize', 15); % put nodes' label in right position.
+    pir(j).NodeLabel={};
 end
